7f92473cc2921702a377e7971e9ebf70b2f6399b
Yuki Hirayama <gfyxm868@gmail.com>
2019-12-07 13:16:39 +0900

    jsx version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-  return <div><h1>Improve React Skill</h1></div> ;
-}
+//function App() {
+ // return <div>Improve React Skill</div> ;
+//}
+class App extends Component {
+	render()
+	{
+	return React.createElement
+			(
+			"div",
+			null,
+			"Hello world!!"
+			);
+			}
 
+		}
 export default App;
